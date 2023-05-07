0084bf91d5475a8a90a808698344aae5cc96402e
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-05-07 19:24:08 -0400

    Jank arrow key controls

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require("express");
 var socket = require("socket.io");
 var { execSync } = require("child_process");
 var getIP = require("./getIP");
-const { mouse, left, right, up, down } = require("@nut-tree/nut-js");
+const { mouse, left, right, up, down, keyboard, Key } = require("@nut-tree/nut-js");
 
 console.log(getIP());
 
@@ -27,16 +27,33 @@ var io = socket(server, {
 
 var gpLink = {
     connectionID: "",
+    inputdata: {"dpad":{"right":false,"up":false,"left":false,"down":false},"joystick1":{"x":0,"y":0},"joystick2":{"x":0,"y":0},"elementpad":{"black":false,"yellow":false,"red":false,"blue":false},"menubutton":false,"xbutton":false,"selectbutton":false},
 };
 io.on("connection", function(socket){
     console.log("made socket connection", socket.id);
     gpLink.connectionID = socket.id;
 
-    socket.on("inputdata", async function(inputdata){
+    socket.on("inputdata", function(inputdata){
         console.log("recieved input data", inputdata);
+        gpLink.inputdata = inputdata;
+    });
+
+    var processInputs = async function() {
+        var inputdata = gpLink.inputdata;
         if (inputdata.joystick2.x != 0 || inputdata.joystick2.y != 0) {
             await mouse.move(right(inputdata.joystick2.x * 100));
             await mouse.move(down(inputdata.joystick2.y * 100));
         }
-    });
+        if (inputdata.dpad.left) await keyboard.pressKey(Key.Left);
+        else await keyboard.releaseKey(Key.Left);
+        if (inputdata.dpad.right) await keyboard.pressKey(Key.Right);
+        else await keyboard.releaseKey(Key.Right);
+        if (inputdata.dpad.up) await keyboard.pressKey(Key.Up);
+        else await keyboard.releaseKey(Key.Up);
+        if (inputdata.dpad.down) await keyboard.pressKey(Key.Down);
+        else await keyboard.releaseKey(Key.Down);
+
+        setTimeout(processInputs, 100); // requestAnimationFrame doesn't wokr
+    };
+    processInputs();
 });
