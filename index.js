f036fe9f375e05df9675cc25afe9779e78159a4f
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-08-16 10:43:04 -0400

    Show URL for gamepad

--- index.js
+++ index.js
@@ -3,5 +3,9 @@
 var DServer = require("./DServer");
 const ip = require("ip");
 
-console.log(ip.address());
+var ipAddress = ip.address();
 new DServer();
+
+console.log("To use gamepad: open",
+    "http://" + ipAddress + ":1999",
+    "on your mobile device");
